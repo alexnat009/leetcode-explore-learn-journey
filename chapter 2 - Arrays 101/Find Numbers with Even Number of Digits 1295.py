95a7a676c8a3b3f97b0d1e428c724cb670a1e39d
alexnat009 <aleqsandre1515@gmail.com>
2025-03-14 15:26:39 +0400

    problem 1295 - Find Numbers with Even Number of Digits

---
+++ chapter 2 - Arrays 101/Find Numbers with Even Number of Digits 1295.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+	def findNumbers(self, nums: List[int]) -> int:
+		return sum([1 for i in nums if len(str(i)) % 2 == 0])
+
+
+if __name__ == "__main__":
+	sol = Solution()
+	nums = [12, 345, 2, 6, 7896]
+
+	print(sol.findNumbers(nums))
