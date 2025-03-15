43d8be806110a7424537f7881e6afa5bcdf1a242
alexnat009 <aleqsandre1515@gmail.com>
2025-03-16 02:31:26 +0400

    Problem 27 - Remove Element Another Solution

--- chapter 2 - Arrays 101/Deleting Items From an Array/remove element 27.py
+++ chapter 2 - Arrays 101/Deleting Items From an Array/remove element 27.py
@@ -3,14 +3,19 @@ from typing import List
 
 class Solution:
 	def removeElement(self, nums: List[int], val: int) -> int:
-		lastId = len(nums) - 1
-		currentId = len(nums) - 1
-		while currentId >= 0:
-			if nums[currentId] == val:
-				nums[lastId], nums[currentId] = nums[currentId], nums[lastId]
-				lastId -= 1
-			currentId -= 1
-		return lastId + 1
+		n = len(nums)
+		slowP = 0
+		fastP = 0
+		while fastP < n:
+			if nums[slowP] != val:
+				slowP += 1
+				fastP += 1
+			else:
+				if nums[fastP] != val:
+					nums[fastP], nums[slowP] = nums[slowP], nums[fastP]
+					slowP += 1
+				fastP += 1
+		return slowP
 
 
 if __name__ == "__main__":
