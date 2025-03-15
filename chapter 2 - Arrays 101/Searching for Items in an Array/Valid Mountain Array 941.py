c3bad3e5646cf356aec11cb8848522d4f44e350d
alexnat009 <aleqsandre1515@gmail.com>
2025-03-15 23:59:04 +0400

    Problem 941 - Valid Mountain Array Another Solution

--- chapter 2 - Arrays 101/Searching for Items in an Array/Valid Mountain Array 941.py
+++ chapter 2 - Arrays 101/Searching for Items in an Array/Valid Mountain Array 941.py
@@ -3,30 +3,32 @@ from typing import List
 
 class Solution:
 	def validMountainArray(self, arr: List[int]) -> bool:
-		if len(arr) < 3:
+		n = len(arr)
+		if n < 3:
 			return False
-		increase = arr[0]
-		startDec = False
-		startInc = False
-		for i in range(1, len(arr)):
-			if arr[i] == arr[i - 1]:
-				return False
-			if arr[i] >= increase:
-				startInc = True
-				if startDec:
-					return False
-				increase = arr[i]
-			elif arr[i] < increase:
-				increase = arr[i]
-				startDec = True
 
-		return True and startDec and startInc
+		idx = -1
+
+		for i in range(n - 1):
+			if arr[i] >= arr[i + 1]:
+				idx = i
+				break
+
+		if idx == 0:
+			return False
+
+		for i in range(idx, n - 1):
+			if arr[i] <= arr[i + 1]:
+				return False
+
+		return True
 
 
 if __name__ == "__main__":
 	sol = Solution()
-	# arr = [0, 2, 3, 4, 5]
+	arr = [1, 2, 3, 4, 5]
 	# arr = [2, 0, 2]
 	# arr = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
-	arr = [1, 7, 9, 5, 4, 1, 2]
+	# arr = [1, 7, 9, 5, 4, 1, 2]
+	arr = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
 	print(sol.validMountainArray(arr))
