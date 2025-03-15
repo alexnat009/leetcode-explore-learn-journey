8265a59d7c34958e891e8bbf4ddbc5f255182ec2
alexnat009 <aleqsandre1515@gmail.com>
2025-03-15 22:30:47 +0400

    Problem 1346 - Check if N and Its Double Exists Another Solution: Set

--- chapter 2 - Arrays 101/Searching for Items in an Array/Check If N and Its Double Exist 1346.py
+++ chapter 2 - Arrays 101/Searching for Items in an Array/Check If N and Its Double Exist 1346.py
@@ -3,14 +3,22 @@ from typing import List
 
 class Solution:
 	def checkIfExist(self, arr: List[int]) -> bool:
-		for i in range(len(arr)):
-			for j in range(i + 1, len(arr)):
-				if arr[i] == arr[j] * 2 or arr[j] == 2 * arr[i]:
-					return True
+		tmpSet = set()
+		for i in arr:
+			if i not in tmpSet:
+				tmpSet.add(i)
+			elif i == 0 and i in tmpSet:
+				return True
+
+			if ((i * 2 in tmpSet) or (i / 2 in tmpSet)) and i != 0:
+				return True
 		return False
+
+
 
 
 if __name__ == "__main__":
 	sol = Solution()
 	arr = [-2, 0, 10, -19, 4, 6, -8]
+
 	print(sol.checkIfExist(arr))
