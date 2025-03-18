6498eac18c82bd24e03858db3d3e7c8762d07c0e
alexnat009 <aleqsandre1515@gmail.com>
2025-03-18 23:57:19 +0400

    Problem 350 - Intersection of Two Arrays II Solution 2: Sort + Two Pointers

--- chapter 3 - Binary Search/More Practises/Intersection of Two Arrays II 350.py
+++ chapter 3 - Binary Search/More Practises/Intersection of Two Arrays II 350.py
@@ -4,11 +4,22 @@ from collections import Counter
 
 class Solution:
 	def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-		cnt1 = Counter(nums1)
-		cnt2 = Counter(nums2)
+		if not nums1 or not nums2:
+			return []
+		nums1.sort()
+		nums2.sort()
+		n1, n2, p1, p2 = len(nums1), len(nums2), 0, 0
 		res = []
-		for key, value in (cnt1 & cnt2).items():
-			res.extend([key for _ in range(value)])
+
+		while p1 < n1 and p2 < n2:
+			if nums1[p1] == nums2[p2]:
+				res.append(nums1[p1])
+				p1 += 1
+				p2 += 1
+			elif nums1[p1] < nums2[p2]:
+				p1 += 1
+			else:
+				p2 += 1
 		return res
 
 
@@ -16,6 +27,6 @@ if __name__ == "__main__":
 	sol = Solution()
 
 	nums1 = [1, 2, 2, 1]
-	nums2 = [2, 2]
+	nums2 = [2, 2, 1]
 
 	print(sol.intersect(nums1, nums2))
