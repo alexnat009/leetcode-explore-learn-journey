42a5b8ab3d4ef8702004dff4755b24d6fc53b119
alexnat009 <aleqsandre1515@gmail.com>
2025-03-23 21:45:45 +0400

    Problem 67: Add Binary Solution with digit summation

--- Chapter 4 - Array and String/Introduction to String/Add Binary 67.py
+++ Chapter 4 - Array and String/Introduction to String/Add Binary 67.py
@@ -1,6 +1,18 @@
 class Solution:
 	def addBinary(self, a: str, b: str) -> str:
-		return str(bin(int(a, 2) + (int(b, 2))))[2:]
+		carry = 0
+		res = []
+
+		a = list(a)
+		b = list(b)
+		while a or b or carry:
+			if a:
+				carry += int(a.pop())
+			if b:
+				carry += int(b.pop())
+			res.append(str(carry % 2))
+			carry //= 2
+		return "".join(res[::-1])
 
 
 if __name__ == "__main__":
