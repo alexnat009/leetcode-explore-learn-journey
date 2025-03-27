cbddc4adc2781d64d20d1e5359dc47e0d2fb30e7
alexnat009 <aleqsandre1515@gmail.com>
2025-03-27 17:26:34 +0400

    Problem 209: Minimum Size Subarray Sum 209 Solution with PrefixSum + Binary Search

--- Chapter 4 - Array and String/Two-Pointer Technique/Minimum Size Subarray Sum 209.py
+++ Chapter 4 - Array and String/Two-Pointer Technique/Minimum Size Subarray Sum 209.py
@@ -4,22 +4,36 @@ from typing import List
 class Solution:
 	def minSubArrayLen(self, target: int, nums: List[int]) -> int:
 		n = len(nums)
-		left = 0
-		currentSum = 0
-		minSubArray = float("inf")
-		for right in range(n):
-			currentSum += nums[right]
-			while currentSum >= target:
-				minSubArray = min(minSubArray, right - left + 1)
-				currentSum -= nums[left]
-				left += 1
-		return minSubArray if minSubArray != float('inf') else 0
+
+		prefixSum = [0] * (n + 1)
+		for i in range(1, n + 1):
+			prefixSum[i] = prefixSum[i - 1] + nums[i - 1]
+
+		left, right = 1, n
+		result = n + 1
+
+		while left <= right:
+			mid = (left + right) // 2
+			if self.isValid(prefixSum, mid):
+				result = mid
+				right = mid - 1
+			else:
+				left = mid + 1
+
+		return result if result != n + 1 else 0
+
+	def isValid(self, prefixSum: List[int], length: int) -> bool:
+		for start in range(len(prefixSum) - length):
+			end = start + length
+			if prefixSum[end] - prefixSum[start] >= target:
+				return True
+		return False
 
 
 if __name__ == "__main__":
 	sol = Solution()
-	target = 6
-	nums = [10, 2, 3]
-	# target = 7
-	# nums = [2, 3, 1, 2, 4, 3]
+	# target = 6
+	# nums = [10, 2, 3]
+	target = 7
+	nums = [2, 3, 1, 2, 4, 3]
 	print(sol.minSubArrayLen(target, nums))
