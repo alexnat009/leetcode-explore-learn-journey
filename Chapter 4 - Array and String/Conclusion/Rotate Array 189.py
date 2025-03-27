9970eb802c5bc41a4d3a88fdc645ac6b927c9bce
alexnat009 <aleqsandre1515@gmail.com>
2025-03-27 18:37:24 +0400

    Problem 189: Rotate Array 189 Solution with Cyclic Replacement

--- Chapter 4 - Array and String/Conclusion/Rotate Array 189.py
+++ Chapter 4 - Array and String/Conclusion/Rotate Array 189.py
@@ -6,25 +6,34 @@ class Solution:
 		"""
 		Do not return anything, modify nums in-place instead.
 		"""
+		n = len(nums)
+		k = k % n
+		count = 0
 
-		k %= len(nums)
-		if k:
-			nums[:] = nums[-k:] + nums[:-k]
-		else:
-			pass
+		start = 0
+		while count < n:
+			current = start
+			prev = nums[start]
+
+			while True:
+				next_id = (current + k) % n
+				nums[next_id], prev = prev, nums[next_id]
+				current = next_id
+				count += 1
+
+				if current == start:
+					break
+			start += 1
+			if count >= n:
+				break
 
 
 if __name__ == "__main__":
 	sol = Solution()
 	# nums = [10, 2, 3]
 
-	nums = [1, 2, 3, 4, 5, 6, 7]
-	k = 3
-	# nums = [-1, -100, 3, 99]
-	# k = 2
-	# nums = [1]
-	# k = 0
-	# nums = [1]
-	# k = 1
+	nums = [-1, -100, 3, 99]
+	k = 2
+
 	print(sol.rotate(nums, k))
 	print(nums)
