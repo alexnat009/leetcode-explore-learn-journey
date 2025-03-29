d3fb59935d08fee87087f1d9743a837e044faf43
alexnat009 <aleqsandre1515@gmail.com>
2025-03-29 19:05:38 +0400

    Problem 622: Design Circular Queue Guide version

--- chapter 5 - Queue & Stack/1 - Queue First-in-first-out Data Structure/Design Circular Queue 622.py
+++ chapter 5 - Queue & Stack/1 - Queue First-in-first-out Data Structure/Design Circular Queue 622.py
@@ -1,42 +1,45 @@
 class MyCircularQueue:
 
 	def __init__(self, k: int):
-		self.queue = []
-		self.k = k
+		self.queue = [-1] * k
+		self.head = -1
+		self.tail = -1
+		self.size = k
 
 	def enQueue(self, value: int) -> bool:
-		if self.k > 0:
-			self.queue.append(value)
-			self.k -= 1
-			return True
-		return False
+		if self.isFull():
+			return False
+		if self.isEmpty():
+			self.head = 0
+		self.tail = (self.tail + 1) % self.size
+		self.queue[self.tail] = value
+		return True
 
 	def deQueue(self) -> bool:
-		if self.queue:
-			self.queue.pop(0)
-			self.k += 1
+		if self.isEmpty():
+			return False
+		if self.head == self.tail:
+			self.head = -1
+			self.tail = -1
 			return True
-		return False
+		self.head = (self.head + 1) % self.size
+		return True
 
 	def Front(self) -> int:
-		if self.queue:
-			return self.queue[0]
-		return -1
+		if self.isEmpty():
+			return -1
+		return self.queue[self.head]
 
 	def Rear(self) -> int:
-		if self.queue:
-			return self.queue[-1]
-		return -1
+		if self.isEmpty():
+			return -1
+		return self.queue[self.tail]
 
 	def isEmpty(self) -> bool:
-		if not self.queue:
-			return True
-		return False
+		return self.head == -1
 
 	def isFull(self) -> bool:
-		if self.k == 0:
-			return True
-		return False
+		return (self.tail + 1) % self.size == self.head
 
 
 if __name__ == "__main__":
