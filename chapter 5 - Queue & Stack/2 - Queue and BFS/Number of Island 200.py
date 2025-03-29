f72ae1d2bd2877021c13eb43ac3d8c3ce6a88f4f
alexnat009 <aleqsandre1515@gmail.com>
2025-03-30 01:58:51 +0400

    Problem 200: Number of Island Solution with transforming visited islands to 0s

--- chapter 5 - Queue & Stack/2 - Queue and BFS/Number of Island 200.py
+++ chapter 5 - Queue & Stack/2 - Queue and BFS/Number of Island 200.py
@@ -8,7 +8,6 @@ class Solution:
 			return 0
 
 		n, m = len(grid), len(grid[0])
-		visited = set()
 		directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 		island_count = 0
 
@@ -17,22 +16,21 @@ class Solution:
 				return 0
 
 			queue = deque([(start_i, start_j)])
-			visited.add((start_i, start_j))
+			grid[start_i][start_j] = "0"
 
 			while queue:
 				current_i, current_j = queue.popleft()
 				for di, dj in directions:
 					next_i, next_j = current_i + di, current_j + dj
-					if 0 <= next_i < n and 0 <= next_j < m and (next_i, next_j) not in visited and grid[next_i][
-						next_j] == "1":
+					if 0 <= next_i < n and 0 <= next_j < m and grid[next_i][next_j] == "1":
+						grid[next_i][next_j] = "0"
 						queue.append((next_i, next_j))
-						visited.add((next_i, next_j))
 
 		for i in range(n):
 			for j in range(m):
-				if grid[i][j] == "1" and (i, j) not in visited:
-					bfs(i, j)
+				if grid[i][j] == "1":
 					island_count += 1
+					bfs(i, j)
 
 		return island_count
 
