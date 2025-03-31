d3c3c025ce1aa00f9ac2181d459c3012c8ba9387
alexnat009 <aleqsandre1515@gmail.com>
2025-03-31 14:47:09 +0400

    Problem 752: Open the Lock Solution with Bidirectional BFS and sets

--- chapter 5 - Queue & Stack/2 - Queue and BFS/Open the Lock 752.py
+++ chapter 5 - Queue & Stack/2 - Queue and BFS/Open the Lock 752.py
@@ -6,35 +6,37 @@ class Solution:
 	def openLock(self, deadends: List[str], target: str) -> int:
 
 		def changes(code):
-			variations = []
 			for i in range(len(code)):
 				for delta in [-1, 1]:
 					new_digit = (int(code[i]) + delta) % 10
 
-					variations.append(code[:i] + str(new_digit) + code[i + 1:])
-			return variations
-
-		def count_steps(start):
-			queue = deque([start])
-			visited = {start}
-			step = 1
-			while queue:
-				size = len(queue)
-				for _ in range(size):
-					current = queue.popleft()
-					for variant in changes(current):
-						if variant == target:
-							return step
-						if variant not in deadends and variant not in visited:
-							queue.append(variant)
-							visited.add(variant)
-				step += 1
-			return -1
+					yield code[:i] + str(new_digit) + code[i + 1:]
 
 		root = "0000"
 		if root == target:
 			return 0
-		return count_steps(root) if root not in deadends else -1
+		deadends = set(deadends)
+		if root in deadends:
+			return -1
+
+		front = {root}
+		back = {target}
+		visited = {root, target}
+		step = 0
+		while front and back:
+			step += 1
+			if len(front) > len(back):
+				front, back = back, front
+			next_level = set()
+			for lock in front:
+				for variants in changes(lock):
+					if variants in back:
+						return step
+					if variants not in visited and variants not in deadends:
+						visited.add(variants)
+						next_level.add(variants)
+			front = next_level
+		return -1
 
 
 if __name__ == "__main__":
