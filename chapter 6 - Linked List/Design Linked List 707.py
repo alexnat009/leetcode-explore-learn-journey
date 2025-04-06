edda2e2fe6328422e3af68f7880839f9c9276405
alexnat009 <aleqsandre1515@gmail.com>
2025-04-07 03:02:41 +0400

    Problem 707: Design Linked List Solution with Singly Linked List

--- chapter 6 - Linked List/Design Linked List 707.py
+++ chapter 6 - Linked List/Design Linked List 707.py
@@ -1,7 +1,6 @@
 class ListNode:
-	def __init__(self, val, prev_node=None, next_node=None):
+	def __init__(self, val, next_node=None):
 		self.val = val
-		self.prev = prev_node
 		self.next = next_node
 
 
@@ -11,43 +10,50 @@ class MyLinkedList:
 		self.left = ListNode(0)
 		self.right = ListNode(0)
 		self.left.next = self.right
-		self.right.prev = self.left
+		self.size = 0
 
 	def get(self, index: int) -> int:
+		if index < 0 or index >= self.size:
+			return -1
 		cur = self.left.next
-		while cur and index > 0:
+		while index > 0:
 			cur = cur.next
 			index -= 1
-		return cur.val if (cur and cur != self.right and index == 0) else -1
+		return cur.val
 
 	def addAtHead(self, val: int) -> None:
-		node = ListNode(val, self.left, self.left.next)
-		self.left.next.prev = node
+		node = ListNode(val, self.left.next)
 		self.left.next = node
+		self.size += 1
 
 	def addAtTail(self, val: int) -> None:
-		node = ListNode(val, self.right.prev, self.right)
-		self.right.prev.next = node
-		self.right.prev = node
+		cur = self.left
+		while cur.next != self.right:
+			cur = cur.next
+		node = ListNode(val, self.right)
+		cur.next = node
+		self.size += 1
 
 	def addAtIndex(self, index: int, val: int) -> None:
-		cur = self.left.next
-		while cur and index > 0:
+		if index < 0 or index > self.size:
+			return
+		cur = self.left
+		while index > 0:
 			cur = cur.next
 			index -= 1
-		if cur and index == 0:
-			node = ListNode(val, cur.prev, cur)
-			cur.prev.next = node
-			cur.prev = node
+		new_node = ListNode(val, cur.next)
+		cur.next = new_node
+		self.size += 1
 
 	def deleteAtIndex(self, index: int) -> None:
-		cur = self.left.next
-		while cur and index > 0:
+		if index < 0 or index >= self.size:
+			return
+		cur = self.left
+		while index > 0:
 			cur = cur.next
 			index -= 1
-		if cur and cur != self.right and index == 0:
-			cur.next.prev = cur.prev
-			cur.prev.next = cur.next
+		cur.next = cur.next.next
+		self.size -= 1
 
 # Your MyLinkedList object will be instantiated and called as such:
 # obj = MyLinkedList()
