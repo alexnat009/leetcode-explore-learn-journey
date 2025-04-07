224573f4a5dbf90d7551319e9872df750ce3040b
alexnat009 <aleqsandre1515@gmail.com>
2025-04-07 16:39:00 +0400

    Problem 142: Linked List Cycle II Solution with Floyd's Cycle-Finding Algorithm

--- chapter 6 - Linked List/2 - Two Pointer Technique/Linked List Cycle II 142.py
+++ chapter 6 - Linked List/2 - Two Pointer Technique/Linked List Cycle II 142.py
@@ -5,21 +5,26 @@ from utils.utils import ListNode
 
 class Solution:
 	def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-		if not head:
+		if not head or not head.next:
 			return None
 
-		ptr = head
-		visited = {head}
-		while ptr and ptr.next:
-			ptr = ptr.next
-			if ptr in visited:
-				return ptr
-			visited.add(ptr)
-		return None
+		ptrSlow = head
+		ptrFast = head
+		while ptrFast and ptrFast.next:
+			ptrSlow = ptrSlow.next
+			ptrFast = ptrFast.next.next
+			if ptrFast == ptrSlow:
+				break
+		else:
+			return None
+		ptrSlow = head
+		while ptrFast != ptrSlow:
+			ptrSlow = ptrSlow.next
+			ptrFast = ptrFast.next
+		return ptrSlow
 
 
 if __name__ == "__main__":
 	sol = Solution()
 	head = [3, 2, 0, -4]
 	pos = 1
-
