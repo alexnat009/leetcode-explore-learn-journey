c209ef37ba60bfaf307ff3db01484077908fe412
alexnat009 <aleqsandre1515@gmail.com>
2025-04-12 00:21:16 +0400

    Problem 206: Reverse Linked List

---
+++ chapter 6 - Linked List/3 - Classic problems/Reverse linked list 206.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+from utils.utils import ListNode
+
+
+class Solution:
+	def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+		if not head:
+			return None
+		prev, curr = None, head
+		while curr:
+			nxt = curr.next
+			curr.next = prev
+			prev = curr
+			curr = nxt
+		return prev
+
