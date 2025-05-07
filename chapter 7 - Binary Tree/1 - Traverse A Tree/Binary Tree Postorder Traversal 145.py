d4c2ea9ddbca545b4871c0ae7f3769e904c769ed
alexnat009 <aleqsandre1515@gmail.com>
2025-05-07 22:18:30 +0400

    Problem 145: Binary Tree Postorder Traversal Solution with stack

--- chapter 7 - Binary Tree/1 - Traverse A Tree/Binary Tree Postorder Traversal 145.py
+++ chapter 7 - Binary Tree/1 - Traverse A Tree/Binary Tree Postorder Traversal 145.py
@@ -7,4 +7,18 @@ class Solution:
 	def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
 		if not root:
 			return []
-		return self.postorderTraversal(root.left) + self.postorderTraversal(root.right) + [root.val]
+		stack1 = [root]
+		stack2 = []
+		result = []
+		while stack1:
+			current = stack1.pop()
+			stack2.append(current)
+
+			if current.left:
+				stack1.append(current.left)
+			if current.right:
+				stack1.append(current.right)
+
+		while stack2:
+			result.append(stack2.pop().val)
+		return result
