4d8a884f422efaed39b025cb0366d1ddfd0ba710
alexnat009 <aleqsandre1515@gmail.com>
2025-05-07 21:58:43 +0400

    Problem 145: Binary Tree Postorder Traversal Solution with recursion

---
+++ chapter 7 - Binary Tree/1 - Traverse A Tree/Binary Tree Postorder Traversal 145.py
@@ -0,0 +1,10 @@
+from typing import Optional, List
+
+from utils.utils import TreeNode
+
+
+class Solution:
+	def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+		if not root:
+			return []
+		return self.postorderTraversal(root.left) + self.postorderTraversal(root.right) + [root.val]
